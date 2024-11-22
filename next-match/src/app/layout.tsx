8117a3e3c021661e5e62a791b421afbde0b78ed4
WolfyLycansDK <raven@lycans.dk>
2024-11-22 21:04:31 +0100

    End of Lecture 9

--- next-match/src/app/layout.tsx
+++ next-match/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Providers from "@/components/Providers";
 
 export const metadata: Metadata = {
   title: "NextMatch",
@@ -16,7 +17,9 @@ export default function RootLayout({
       <body
         className={`antialiased`}
       >
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- next-match/src/app/page.tsx
+++ next-match/src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@nextui-org/react";
+import { FaRegSmile } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl">Hello App!!</h1>
+      <Button color='primary' variant='bordered' startContent={<FaRegSmile size={20} />}>
+        Click Me!
+      </Button>
     </div>
   );
 }
