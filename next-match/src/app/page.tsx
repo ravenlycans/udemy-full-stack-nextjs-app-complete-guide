57f49ff19b9a5a166c5dd82b5ae27f892e2fa739
WolfyLycansDK <raven@lycans.dk>
2024-11-22 21:16:49 +0100

    End of lecture 10

--- next-match/src/app/page.tsx
+++ next-match/src/app/page.tsx
@@ -1,11 +1,18 @@
 import { Button } from "@nextui-org/react";
+import Link from "next/link";
 import { FaRegSmile } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl">Hello App!!</h1>
-      <Button color='primary' variant='bordered' startContent={<FaRegSmile size={20} />}>
+      <Button 
+        color='primary' 
+        variant='bordered' 
+        startContent={<FaRegSmile size={20} />}
+        as={Link}
+        href='/members'
+      >
         Click Me!
       </Button>
     </div>
