afc47407b91e590f38c5b327268047f605ecfdeb
WolfyLycansDK <raven@lycans.dk>
2024-11-22 21:32:26 +0100

    End of Lecture 11

--- next-match/src/app/layout.tsx
+++ next-match/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "@/components/Providers";
+import TopNav from "@/components/navbar/TopNav";
 
 export const metadata: Metadata = {
   title: "NextMatch",
@@ -18,6 +19,7 @@ export default function RootLayout({
         className={`antialiased h-screen`}
       >
         <Providers>
+          <TopNav />
           {children}
         </Providers>
       </body>
