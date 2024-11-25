5a9562000fb9a7fdad02ba6f66522b58394c232f
WolfyLycansDK <raven@lycans.dk>
2024-11-25 10:00:46 +0100

    End of lecture 17

---
+++ next-match/src/app/(auth)/login/LoginForm.tsx
@@ -0,0 +1,38 @@
+import { Button, Card, CardBody, CardHeader, Input } from "@nextui-org/react";
+import { GiPadlock } from "react-icons/gi";
+
+export default function LoginForm() {
+  return (
+    <>
+        <Card className="w-2/5 mx-auto">
+            <CardHeader className="flex flex-col items-center justify-center">
+                <div className="flex flex-col gap-2 items-center text-secondary">
+                    <div className="flex flex-row items-center gap-3">
+                        <GiPadlock size={30} />
+                        <h1 className="text-3xl font-semibold">Login</h1>
+                    </div>
+                    <p className="text-neutral-500">Welcome back to NextMatch</p>
+                </div>
+            </CardHeader>
+            <CardBody>
+                <form action="">
+                    <div className="space-y-4">
+                        <Input
+                            label="Email"
+                            variant="bordered"
+                        />
+                        <Input
+                            label="Password"
+                            variant="bordered"
+                            type="password"
+                        />
+                        <Button fullWidth color="secondary" type="submit">
+                            Login
+                        </Button>
+                    </div>
+                </form>
+            </CardBody>
+        </Card>
+    </>
+  )
+}
