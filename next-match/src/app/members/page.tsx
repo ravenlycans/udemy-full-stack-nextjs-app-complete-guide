2e84c3a52fe3e8912ae8481911479b72c76c1221
WolfyLycansDK <raven@lycans.dk>
2024-11-26 13:44:34 +0100

    End of lecture 42

--- next-match/src/app/members/page.tsx
+++ next-match/src/app/members/page.tsx
@@ -1,10 +1,15 @@
-import Link from "next/link";
+import { getMembers } from "../actions/memberActions";
 
-export default function MembersPage() {
+export default async function MembersPage() {
+  const members = await getMembers();
+
   return (
     <div>
-      <h3 className='text-3xl'>This will be the members page.</h3>
-      <Link href='/'>Go Back Home</Link>
+      <ul>
+        {members && members.map(member => (
+          <li key={member.id}>{member.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
