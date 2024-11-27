2f46225666a178dd804699a251dc8e05c942a38a
WolfyLycansDK <raven@lycans.dk>
2024-11-27 14:21:41 +0100

    End of lecture 73

--- next-match/src/app/actions/userActions.ts
+++ next-match/src/app/actions/userActions.ts
@@ -1,58 +1,85 @@
-'use server';
+"use server";
 
-import { memberEditSchema, MemberEditSchema } from "@/lib/schemas/memberEditSchema";
+import {
+  memberEditSchema,
+  MemberEditSchema,
+} from "@/lib/schemas/memberEditSchema";
 import { ActionResult } from "@/types";
 import { getAuthUserId } from "./authActions";
 import { prisma } from "@/lib/prisma";
-import { Member } from "@prisma/client";
+import { Member, Photo } from "@prisma/client";
 
-export async function updateMemberProfile(data: MemberEditSchema): Promise<ActionResult<Member>> {
-    try {
-        const userId = await getAuthUserId();
+export async function updateMemberProfile(
+  data: MemberEditSchema
+): Promise<ActionResult<Member>> {
+  try {
+    const userId = await getAuthUserId();
 
-        const validated = memberEditSchema.safeParse(data);
+    const validated = memberEditSchema.safeParse(data);
 
-        if (!validated.success) return {status: 'error', error: validated.error.errors}
+    if (!validated.success)
+      return { status: "error", error: validated.error.errors };
 
-        const {name, description, city, country} = validated.data;
+    const { name, description, city, country } = validated.data;
 
-        const member = await prisma.member.update({
-            where: {userId},
-            data: {
-                name,
-                description,
-                city,
-                country,
-            }
-        });
+    const member = await prisma.member.update({
+      where: { userId },
+      data: {
+        name,
+        description,
+        city,
+        country,
+      },
+    });
 
-        return {status: 'success', data: member}
-    } catch (error) {
-        console.log(error);
+    return { status: "success", data: member };
+  } catch (error) {
+    console.log(error);
 
-        return {status: 'error', error: 'Something went wrong.'}
-    }
+    return { status: "error", error: "Something went wrong." };
+  }
 }
 
 export async function addImage(url: string, publicId: string) {
-    try {
-        const userId = await getAuthUserId();
+  try {
+    const userId = await getAuthUserId();
 
-        return prisma.member.update({
-            where: {userId},
-            data: {
-                photos: {
-                    create: [
-                        {
-                            url,
-                            publicId,
-                        }
-                    ]
-                }
-            }
-        });
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+    return prisma.member.update({
+      where: { userId },
+      data: {
+        photos: {
+          create: [
+            {
+              url,
+              publicId,
+            },
+          ],
+        },
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function setMainImage(photo: Photo) {
+  try {
+    const userId = await getAuthUserId();
+
+    await prisma.user.update({
+      where: { id: userId },
+      data: { image: photo.url },
+    });
+
+    return prisma.member.update({
+      where: { userId },
+      data: {
+        image: photo.url,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
--- next-match/src/app/members/edit/photos/page.tsx
+++ next-match/src/app/members/edit/photos/page.tsx
@@ -1,14 +1,17 @@
 import { getAuthUserId } from "@/app/actions/authActions";
-import { getMemberPhotosByUserId } from "@/app/actions/memberActions";
-import DeleteButton from "@/components/DeleteButton";
-import StarButton from "@/components/StarButton";
+import {
+  getMemberByUserId,
+  getMemberPhotosByUserId,
+} from "@/app/actions/memberActions";
 import { CardHeader, Divider, CardBody } from "@nextui-org/react";
 import MemberPhotoUpload from "./MemberPhotoUpload";
-import MemberImage from "@/components/MemberImage";
+
+import MemberPhotos from "@/components/MemberPhotos";
 
 export default async function PhotosPage() {
   const userId = await getAuthUserId();
-  
+  const member = await getMemberByUserId(userId);
+
   const photos = await getMemberPhotosByUserId(userId);
 
   return (
@@ -19,21 +22,11 @@ export default async function PhotosPage() {
       <Divider />
       <CardBody>
         <MemberPhotoUpload />
-        <div className="grid grid-cols-3 lg:grid-cols-5 gap-3 p-5">
-            {photos && photos.map(photo => (
-                <div key={photo.id} className="relative">
-                    <MemberImage
-                      photo={photo}
-                    />
-                    <div className="absolute top-3 left-3 z-50">
-                      <StarButton selected={false} loading={false} />
-                    </div>
-                    <div className="absolute top-3 right-3 z-50">
-                      <DeleteButton loading={false} />
-                    </div>
-                </div>
-            ))}
-        </div>
+        <MemberPhotos
+          photos={photos}
+          editing={true}
+          mainImageUrl={member?.image}
+        />
       </CardBody>
     </>
   );
