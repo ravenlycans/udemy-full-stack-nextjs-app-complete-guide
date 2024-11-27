d3f34fe41a80d0c0a5f0d3637a6da1b4fd21b984
WolfyLycansDK <raven@lycans.dk>
2024-11-27 10:10:31 +0100

    End of Lecture 55

--- next-match/src/app/members/MemberCard.tsx
+++ next-match/src/app/members/MemberCard.tsx
@@ -1,3 +1,4 @@
+import LikeButton from "@/components/LikeButton"
 import { calculateAge } from "@/lib/util"
 import { Card, CardFooter, Image } from "@nextui-org/react"
 import { Member } from "@prisma/client"
@@ -22,6 +23,9 @@ export default function MemberCard({member}: Props) {
             src={member.image || '/images/user.png'}
             className='aspect-square object-cover'
         />
+        <div className="absolute top-3 right-3 z-50">
+            <LikeButton targetId={member.userId} hasLiked={false} />
+        </div>
         <CardFooter className="flex justify-start bg-black bg-dark-gradient overflow-hidden absolute bottom-0 z-10">
             <div className="flex flex-col text-white">
                 <span className="font-semibold">{member.name}, {calculateAge(member.dateOfBirth)}</span>
