8a17d0fb849c1345009fe63ead1e3a5f0fd4aef9
WolfyLycanDK <raven@lycans.dk>
2024-11-28 10:06:58 +0100

    End of lecture 76

--- next-match/src/components/MemberPhotos.tsx
+++ next-match/src/components/MemberPhotos.tsx
@@ -5,7 +5,7 @@ import MemberImage from "./MemberImage";
 import StarButton from "./StarButton";
 import DeleteButton from "./DeleteButton";
 import { useRouter } from "next/navigation";
-import { setMainImage } from "@/app/actions/userActions";
+import { deleteImage, setMainImage } from "@/app/actions/userActions";
 import { useState } from "react";
 
 type Props = {
@@ -33,6 +33,14 @@ export default function MemberPhotos({ photos, editing, mainImageUrl }: Props) {
     setLoading({ isLoading: false, id: "", type: "" });
   };
 
+  const onDelete = async (photo: Photo) => {
+    if (photo.url === mainImageUrl) return null;
+    setLoading({isLoading: true, id: photo.id, type:"delete"});
+    await deleteImage(photo);
+    router.refresh();
+    setLoading({isLoading: false, id: '', type: ''});
+  }
+
   return (
     <div className="grid grid-cols-3 lg:grid-cols-5 gap-3 p-5">
       {photos &&
@@ -49,8 +57,10 @@ export default function MemberPhotos({ photos, editing, mainImageUrl }: Props) {
                     } 
                   />
                 </div>
-                <div className="absolute top-3 right-3 z-50">
-                  <DeleteButton loading={false} />
+                <div onClick={() => onDelete(photo)} className="absolute top-3 right-3 z-50">
+                  <DeleteButton loading={
+                    loading.isLoading && loading.type === 'delete' && loading.id === photo.id
+                  } />
                 </div>
               </>
             )}
