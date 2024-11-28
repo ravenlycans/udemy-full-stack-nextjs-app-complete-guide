41354040d663774f060e3743679260e99b575f24
WolfyLycanDK <raven@lycans.dk>
2024-11-28 12:51:27 +0100

    End of Lecture 82

---
+++ next-match/src/app/actions/messageActions.ts
@@ -0,0 +1,32 @@
+'use server';
+
+import { messageSchema, MessageSchema } from "@/lib/schemas/messageSchema";
+import { ActionResult } from "@/types";
+import { Message } from "@prisma/client";
+import { getAuthUserId } from "./authActions";
+import { prisma } from "@/lib/prisma";
+
+export async function createMessage(recipientUserId: string, data: MessageSchema): Promise<ActionResult<Message>> {
+    try {
+        const userId = await getAuthUserId();
+
+        const validated = messageSchema.safeParse(data);
+
+        if (!validated.success) return {status: 'error', error: validated.error.errors}
+
+        const {text} = validated.data;
+
+        const message = await prisma.message.create({
+            data: {
+                text,
+                recipientId: recipientUserId,
+                senderId: userId
+            }
+        })
+
+        return {status: 'success', data: message}
+    } catch (error) {
+        console.log(error);
+        return {status: 'error', error: 'Something went wrong'}
+    }
+}
--- next-match/src/app/members/[userId]/chat/ChatForm.tsx
+++ next-match/src/app/members/[userId]/chat/ChatForm.tsx
@@ -1,49 +1,69 @@
 "use client";
 
+import { createMessage } from "@/app/actions/messageActions";
 import { MessageSchema, messageSchema } from "@/lib/schemas/messageSchema";
+import { handleFormServerErrors } from "@/lib/util";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Input } from "@nextui-org/react";
+import { useParams, useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { HiPaperAirplane } from "react-icons/hi2";
 
 export default function ChatForm() {
+  const router = useRouter();
+  const params = useParams<{userId: string}>();
   const {
     register,
     handleSubmit,
     reset,
+    setError,
     formState: { isSubmitting, isValid, errors },
   } = useForm<MessageSchema>({
     resolver: zodResolver(messageSchema),
   });
 
-  const onSubmit = (data: MessageSchema) => {
-    console.log(data);
-    reset();
+  const onSubmit = async (data: MessageSchema) => {
+    const {userId} = await params;
+    const result = await createMessage(userId, data);
+
+    if (result.status === 'error') {
+      handleFormServerErrors(result, setError);
+    } else {
+      reset();
+      router.refresh();
+    }
   };
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="w-full flex items-center gap-2"
+      className="w-full"
     >
-      <Input
-        fullWidth
-        placeholder="Type a message"
-        variant="faded"
-        {...register("text")}
-        isInvalid={!!errors.text}
-        errorMessage={errors.text?.message}
-      />
-      <Button
-        type="submit"
-        isIconOnly
-        color="secondary"
-        radius="full"
-        isLoading={isSubmitting}
-        isDisabled={!isValid || isSubmitting}
-      >
-        <HiPaperAirplane size={18} />
-      </Button>
+      <div className="flex items-center gap-2">
+        <Input
+          fullWidth
+          placeholder="Type a message"
+          variant="faded"
+          {...register("text")}
+          isInvalid={!!errors.text}
+          errorMessage={errors.text?.message}
+        />
+        <Button
+          type="submit"
+          isIconOnly
+          color="secondary"
+          radius="full"
+          isLoading={isSubmitting}
+          isDisabled={!isValid || isSubmitting}
+        >
+          <HiPaperAirplane size={18} />
+        </Button>
+      </div>
+      <div className="flex flex-col">
+      {errors.root?.serverError && (
+        <p className="text-danger text-sm flex flex-col items-center">{errors.root.serverError.message}</p>
+      )}
+      </div>
     </form>
   );
 }
