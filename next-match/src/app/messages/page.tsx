1305509b3770ac01c83fb2c35cbdcb612bf64a6c
WolfyLycansDK <raven@lycans.dk>
2024-11-29 10:15:58 +0100

    End of LEcture 89

--- next-match/src/app/messages/page.tsx
+++ next-match/src/app/messages/page.tsx
@@ -1,14 +1,22 @@
+import { getMessagesByContainer } from "../actions/messageActions";
 import MessageSidebar from "./MessageSidebar";
 
-export default function MessagesPage() {
+export default async function MessagesPage({
+  searchParams,
+}: {
+  searchParams: { container: string };
+}) {
+  const {container} = await searchParams;
+  const messages = await getMessagesByContainer(container);
+
+  console.log({ messages });
+
   return (
     <div className="grid grid-cols-12 gap-5 h-[80vh] mt-10">
       <div className="col-span-2">
         <MessageSidebar />
       </div>
-      <div className="col-span-10">
-        Message Table Go Here.
-      </div>
+      <div className="col-span-10">Message Table Go Here.</div>
     </div>
-  )
+  );
 }
