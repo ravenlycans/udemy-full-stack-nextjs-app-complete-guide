4e5c2d33f6e497e368f0432bb6291faea00dba16
Kim Andersen <wolfy@lycans.dk>
2024-11-29 13:06:23 +0100

    End of lecture 91

--- next-match/src/app/messages/MessageTable.tsx
+++ next-match/src/app/messages/MessageTable.tsx
@@ -1,59 +1,80 @@
-'use client';
+"use client";
 
 import { MessageDTO } from "@/types";
-import { Card, getKeyValue, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
+import {
+  Card,
+  getKeyValue,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Key } from "react";
 
-
 type Props = {
-    messages: MessageDTO[]
-}
+  messages: MessageDTO[];
+};
 
-export default function MessageTable({messages}: Props) {
-    const searchParams = useSearchParams();
-    const router = useRouter();
+export default function MessageTable({ messages }: Props) {
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
-    const isOutbox = searchParams.get('container') === 'outbox';
+  const isOutbox = searchParams.get("container") === "outbox";
 
-    const columns = [
-        {key: isOutbox ? 'recipientName' : 'senderName', label: isOutbox ? 'Recipient' : 'Sender'},
-        {key: 'text', label: 'Message'},
-        {key: 'createdAt', label: isOutbox ? 'Date sent' : 'Date Received'},
-    ]
+  const columns = [
+    {
+      key: isOutbox ? "recipientName" : "senderName",
+      label: isOutbox ? "Recipient" : "Sender",
+    },
+    { key: "text", label: "Message" },
+    { key: "createdAt", label: isOutbox ? "Date sent" : "Date Received" },
+  ];
 
-    const handleRowSelect = (key: Key) => {
-        const message = messages.find(m => m.id === key);
-        const url = isOutbox ? `/members/${message?.recipientId}` : `/members/${message?.senderId}`;
-        router.push(url + '/chat');
-    }
+  const handleRowSelect = (key: Key) => {
+    const message = messages.find((m) => m.id === key);
+    const url = isOutbox
+      ? `/members/${message?.recipientId}`
+      : `/members/${message?.senderId}`;
+    router.push(url + "/chat");
+  };
 
   return (
-    <Card className="flex flex-col gap-3 h-[80vh] overflow-auto">
-        <Table
-            aria-label='table with messages'
-            selectionMode='single'
-            onRowAction={(key) => handleRowSelect(key)}
-            shadow="none"
+    <Card className='flex flex-col gap-3 h-[80vh] overflow-auto'>
+      <Table
+        aria-label='table with messages'
+        selectionMode='single'
+        onRowAction={(key) => handleRowSelect(key)}
+        shadow='none'
+      >
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.key}>{column.label}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody
+          items={messages}
+          emptyContent='No messages for this container'
         >
-            <TableHeader columns={columns}>
-                {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-            </TableHeader>
-            <TableBody items={messages} emptyContent='No messages for this container'>
-                {(item) => (
-                    <TableRow
-                        key={item.id}
-                        className="cursor-pointer"
-                    >
-                        {(columnKey) => (
-                            <TableCell>
-                                {getKeyValue(item, columnKey)}
-                            </TableCell>
-                        )}
-                    </TableRow>
-                )}
-            </TableBody>
-        </Table>
+          {(item) => (
+            <TableRow key={item.id} className='cursor-pointer'>
+              {(columnKey) => (
+                <TableCell>
+                  <div
+                    className={`${
+                      !item.dateRead && !isOutbox ? "font-semibold" : ""
+                    }`}
+                  >
+                    {getKeyValue(item, columnKey)}
+                  </div>
+                </TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
     </Card>
-  )
+  );
 }
