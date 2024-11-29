5e062360d23e449f46e4e8fd633f4aa770a9ea11
Kim Andersen <wolfy@lycans.dk>
2024-11-29 13:31:27 +0100

    End of lecture 93

--- next-match/src/app/messages/MessageTable.tsx
+++ next-match/src/app/messages/MessageTable.tsx
@@ -52,11 +52,7 @@ export default function MessageTable({ messages }: Props) {
         case "recipientName":
         case "senderName":
           return (
-            <div
-              className={`flex item-center gap-2 cursor-pointer ${
-                !item.dateRead && !isOutbox ? "font-semibold" : ""
-              }`}
-            >
+            <div className='flex item-center gap-2 cursor-pointer'>
               <Avatar
                 alt='Image of member'
                 src={
@@ -102,7 +98,11 @@ export default function MessageTable({ messages }: Props) {
           {(item) => (
             <TableRow key={item.id} className='cursor-pointer'>
               {(columnKey) => (
-                <TableCell>
+                <TableCell
+                  className={`${
+                    !item.dateRead && !isOutbox ? "font-semibold" : ""
+                  }`}
+                >
                   {renderCell(item, columnKey as keyof MessageDTO)}
                 </TableCell>
               )}
