961ca9c32f9ed1e099e05d899cb67b35d7f9d5b1
Kim Andersen <wolfy@lycans.dk>
2024-12-02 10:37:12 +0100

    End of lecture 96 and 97

---
+++ next-match/src/lib/pusher.ts
@@ -0,0 +1,28 @@
+import PusherServer from 'pusher';
+import PusherClient from 'pusher-js';
+
+declare global {
+    // eslint-disable-next-line no-var
+    var pusherServerInstance: PusherServer | undefined;
+    // eslint-disable-next-line no-var
+    var pusherClientInstance: PusherClient | undefined;
+}
+
+if (!global.pusherServerInstance) {
+    global.pusherServerInstance = new PusherServer({
+        appId: process.env.PUSHER_APP_ID!,
+        key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+        secret: process.env.PUSHER_SECRET!,
+        cluster: 'eu',
+        useTLS: true
+    });
+}
+
+if (!global.pusherClientInstance) {
+    global.pusherClientInstance = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
+        cluster: 'eu',
+    });
+}
+
+export const pusherServer = global.pusherServerInstance;
+export const pusherClient = global.pusherClientInstance;
