948e81c3234d9fb4cac545aadf47159dc181a675
WolfyLycanDK <raven@lycans.dk>
2024-12-03 10:35:36 +0100

    End of Lecture 106

--- next-match/src/app/members/MemberCard.tsx
+++ next-match/src/app/members/MemberCard.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import LikeButton from "@/components/LikeButton"
+import PresenceDot from "@/components/PresenceDot";
 import { calculateAge, transformImageUrl } from "@/lib/util"
 import { Card, CardFooter, Image } from "@nextui-org/react"
 import { Member } from "@prisma/client"
@@ -36,6 +37,9 @@ export default function MemberCard({member, likeIds}: Props) {
         <div onClick={preventLinkAction}>
             <div className="absolute top-3 right-3 z-50">
                 <LikeButton targetId={member.userId} hasLiked={hasLiked} />
+            </div>
+            <div className='absolute top-2 left-3 z-50'>
+                <PresenceDot member={member} />
             </div>
         </div>
         <CardFooter className="flex justify-start bg-black bg-dark-gradient overflow-hidden absolute bottom-0 z-10">
